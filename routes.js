93736a8f8bfe253c4af76c29cf025f5ffc3a50d7
EzhkaPro <gala-slim@yandex.ru>
2023-08-14 17:40:10 +0300

    posts

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 // Файл со списком страниц приложения
-export const POSTS_PAGE = "posts";
-export const USER_POSTS_PAGE = "user-posts";
-export const AUTH_PAGE = "auth";
-export const ADD_POSTS_PAGE = "add-post";
-export const LOADING_PAGE = "loading";
+export const POSTS_PAGE = "posts";//страница постов
+export const USER_POSTS_PAGE = "user-posts";//страница пользователя
+export const AUTH_PAGE = "auth";//авторизация
+export const ADD_POSTS_PAGE = "add-post";//добавить пост
+export const LOADING_PAGE = "loading";//регистрация
